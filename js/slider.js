c2656009a277105f9105cae1cd879b84fc1eb959
Yuraul <yuraulio@gmail.com>
2020-08-06 16:29:39 +0300

    Зробив слайдер.

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+function slideIt() {
+  for (let item of document.getElementsByClassName('slides')) {
+    item.classList.toggle('transparent');
+    }
+}
+
+for (let item of document.getElementsByClassName('slider_buttons')) {
+  item.onmousedown = function() {
+    event.target.style.opacity = 0.4;
+    slideIt();
+    clearInterval(timer);
+    timer = setInterval(slideIt, 4000);
+  }
+  item.onmouseup = function() {
+    event.target.style.opacity = 1;
+  }
+}
+
+let timer = setInterval(slideIt, 4000);
