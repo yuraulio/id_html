e4ff307db0dc199912f666f6210491ea01bf980f
Yuraul <yuraulio@gmail.com>
2020-08-13 14:55:15 +0300

    Ініціалізація середовища розрозбки

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const {src, dest, watch} = require('gulp');
+var gulp_scss = require('gulp-scss');
+
+function scss() {
+  return src('app/scss/style.scss')
+    .pipe(gulp_scss())
+    .pipe(dest('app/css'));
+
+}
+function  build() {
+  return src('app/**/*')
+    .pipe(dest('dist'));
+}
+
+exports.scss = scss;
+exports.build = build;
+exports.default = function() {
+  watch('app/scss/style.scss', scss);
+}
