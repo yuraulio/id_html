3824dafd42598f42ddcbb6b14c5f63de1cca26b5
Yuraul <yuraulio@gmail.com>
2020-08-15 22:21:14 +0300

    Зробив адаптивну шапку для десктопа і мобільного. Змінив підключення файлу з брейкопоінтами.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const {src, dest, watch} = require('gulp');
 var gulp_scss = require('gulp-scss');
 
 function scss() {
-  return src('app/scss/*.scss')
+  return src('app/scss/style.scss')
     .pipe(gulp_scss())
     .pipe(dest('app/css'));
 
@@ -15,5 +15,5 @@ function  build() {
 exports.scss = scss;
 exports.build = build;
 exports.default = function() {
-  watch('app/scss/style.scss', scss);
+  watch('app/scss/*.scss', scss);
 }
