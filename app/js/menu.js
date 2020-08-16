e75dbd559f6d3892f315213b3d57b5bb8e6e0d09
Yuraul <yuraulio@gmail.com>
2020-08-16 19:27:31 +0300

    Зробив випадаюче меню в мобільному режимі.

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,3 +1,9 @@
-let style = window.getComputedStyle(document.querySelector('html body .wrap_header header nav ul'));
+let menu = document.querySelector('html body .wrap_header header nav');
 
-document.getElementById('menu_button').onclick = () => {console.log('hello');};
+document.getElementById('menu_button').onclick = () => {
+  if (menu.classList.contains('open_menu')) {
+    menu.classList.remove('open_menu');
+  } else {
+    menu.classList.add('open_menu');
+  }
+}
