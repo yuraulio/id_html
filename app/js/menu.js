15b551d3bf14e1deda032f565d1a1dd70dd446bb
Yuraul <yuraulio@gmail.com>
2020-08-16 16:56:28 +0300

    Додав подію клік на кнопку меню і замінив селектор на айді.

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,16 +1,3 @@
 let style = window.getComputedStyle(document.querySelector('html body .wrap_header header nav ul'));
 
-function myClick() {
-  console.log('hello');
-}
-
-let el = document.querySelector('html body .wrap_header header > button');
-el.onclick = myClick;
-
-// document.getElementById('clickme').onclick = myClick;
-
-// console.log(document.querySelector('html body .wrap_header header nav button').tagName);
-
-//
-// document.querySelector('html body .wrap_header header button').addEventListener("click", myClick());
-console.log(style.getPropertyValue('display'));
+document.getElementById('menu_button').onclick = () => {console.log('hello');};
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const {src, dest, watch} = require('gulp');
 var gulp_scss = require('gulp-scss');
+var notify = require('gulp-notify');
 
 function scss() {
   return src('app/scss/style.scss')
-    .pipe(gulp_scss().on('error', function(error)
+    .pipe(gulp_scss().on('error', notify.onError(
       {
-        console.log( error );
-      } ))
+        message: "<%= error.message %>",
+        title  : "SCSS cimpile Error!"
+      })))
     .pipe(dest('app/css'));
-
 }
+
 function  build() {
   return src('app/**/*')
     .pipe(dest('dist'));
