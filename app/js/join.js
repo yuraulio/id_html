d8a4a1ea34bb74133dafb312a0c1d47de593c361
Yuraul <yuraulio@gmail.com>
2020-08-22 22:07:24 +0300

    Перевиставив блок з курсами.

--- app/js/join.js
+++ app/js/join.js
@@ -1,3 +1,3 @@
-document.querySelector('html body .wrap_header header nav ul li button').onclick = () => {
+document.querySelector('html body .header header nav ul li button').onclick = () => {
   document.getElementById('name').focus();
 }
