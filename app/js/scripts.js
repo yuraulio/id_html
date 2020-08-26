397ad35731e9329064f584eb8e9254d336e49054
Yuraul <yuraulio@gmail.com>
2020-08-26 13:53:43 +0300

    В процесі прироблювання слайдера згідно ТЗ в блок dev.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -13897,16 +13897,29 @@ document.querySelector('html body .header header nav ul li button').onclick = ()
   document.getElementById('name').focus();
 }
 
-if (document.getElementsByClassName('dev_item').length > 3) {
-  $(document).ready(function(){
-    $('.dev_items').slick({
-      dots: true,
-      infinite: true,
-      arrows: false,
-      slidesToShow: 3
-    });
+$(document).ready(function() {
+  $('.dev_items').slick({
+    draggable: false,
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      }
+
+      }, {
+
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 2,
+      }
+
+      }]
   });
-}
+});
 
 $(document).ready(function(){
   $('.cites').slick({
--- app/js/src/05_dev.js
+++ app/js/src/05_dev.js
@@ -1,10 +1,23 @@
-if (document.getElementsByClassName('dev_item').length > 3) {
-  $(document).ready(function(){
-    $('.dev_items').slick({
-      dots: true,
-      infinite: true,
-      arrows: false,
-      slidesToShow: 3
-    });
+$(document).ready(function() {
+  $('.dev_items').slick({
+    draggable: false,
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      }
+
+      }, {
+
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 2,
+      }
+
+      }]
   });
-}
+});
