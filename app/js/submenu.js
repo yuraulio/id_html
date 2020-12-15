c6f404a981363f70a192a924c056c7e3c5006b10
Yuraul <yuraulio@gmail.com>
2020-12-15 11:09:38 +0200

    Final commit

---
+++ app/js/submenu.js
@@ -0,0 +1,6 @@
+document.getElementById('sublist_link').onmouseover = () => {
+  document.getElementById('sublist').style.display = 'block';
+}
+document.getElementById('sublist_link').onmouseout = () => {
+  document.getElementById('sublist').style.display = 'none';
+}
--- js/submenu.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('sublist_link').onmouseover = () => {
-  document.getElementById('sublist').style.display = 'block';
-}
-document.getElementById('sublist_link').onmouseout = () => {
-  document.getElementById('sublist').style.display = 'none';
-}
